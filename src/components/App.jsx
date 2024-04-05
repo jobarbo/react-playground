ac037adc1b8e7bb3da969932a665e0c45605a157
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-04-05 18:16:55 -0400

    grid + images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import "./styles/App.css";
 import Header from "./Header";
+import Grid from "./Grid";
 
-let name = "JBarbeau.art";
+let name = "VOIDWARDEN (BETA) by JBarbeau.art";
 let description =
-	"Jonathan Barbeau (b. 1989, Montreal) is an international artist making generative art since 2017. He develops custom systems and algorithms for creating visually striking art, blending logic and chaos. His work is exhibited and collected globally.";
+	"VOIDWARDEN is a generative project that uses particle physics to create unique, abstract art. Each piece is a snapshot of a moment in time, a glimpse into the void. The project is in beta and will feature more variations and options in the future.";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<div className='app'>
-				<h1 className='app__title'>{name}</h1>
-				<p className='app__text'>{description}</p>
+				<div className='app__intro'>
+					<h1 className='app__title'>{name}</h1>
+					<p className='app__text'>{description}</p>
+				</div>
+				<Grid />
 			</div>
 		</>
 	);
---
+++ src/components/Grid.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import "./styles/Grid.css";
+
+const gallery = Object.values(import.meta.glob("../assets/voidwarden/*.{png,jpg,jpeg,PNG,JPG,JPEG}", {eager: true, as: "url"}));
+
+function Grid() {
+	const [selectedImage, setSelectedImage] = useState(null);
+
+	const handleClick = (image) => {
+		setSelectedImage(image);
+		// Add class to app__overlay to show the overlay
+		document.querySelector(".app__overlay").classList.add("app__overlay--show");
+	};
+
+	const handleClose = () => {
+		// Remove class from app__overlay to hide the overlay
+		document.querySelector(".app__overlay").classList.remove("app__overlay--show");
+		// add a delay to remove the selected image
+		setTimeout(() => {
+			setSelectedImage(null);
+		}, 500);
+	};
+
+	return (
+		<>
+			<div className='app__grid'>
+				{gallery.map((image, index) => (
+					<div className='app__grid-item' key={index} onClick={() => handleClick(image)}>
+						<p className='app__grid-index'>{index}</p>
+						<img src={image} alt={`VOIDWARDEN ${index}`} className='grid__image' />
+					</div>
+				))}
+			</div>
+			<div className='app__overlay'>
+				<div className='app__overlay-content'>
+					<img src={selectedImage} alt='Selected Image' className='app__overlay-image' />
+				</div>
+				<span className='app__overlay-close' onClick={handleClose}>
+					Close the image
+				</span>
+			</div>
+		</>
+	);
+}
+
+export default Grid;
